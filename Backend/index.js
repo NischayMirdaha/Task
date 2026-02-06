f9d175f5cc2d77aff1398a681661be217cb428f8
NischayMirdaha <nischaymirdaha@gmail.com>
2026-02-06 21:44:05 +0545

    Land feature ajai

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
 import userroute from "./route/userroute.js";
-
+import landroute from "./route/landRoutes.js";
 dotenv.config();
 
 const app = express();
@@ -13,6 +13,8 @@ app.use(express.json());
 
 // routes
 app.use("/api/auth/user", userroute);
+app.use("/api/land", landroute );
+
 
 app.get("/", (req, res) => {
   res.send("API running 🚀");
