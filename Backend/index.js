3b1ed57bfdb2ec37ce790036fde8eebd58bf4982
NischayMirdaha <nischaymirdaha@gmail.com>
2026-03-04 20:53:08 +0545

    scan start

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
 import userroute from "./route/userroute.js";
+import scanRoutes from "./route/scanRoutes.js";
 // import landroute from "./route/landRoutes.js";
 dotenv.config();
 
@@ -14,7 +15,7 @@ app.use(express.json());
 // routes
 app.use("/api/auth/user", userroute);
 // app.use("/api/land", landroute );
-
+app.use("/api", scanRoutes);
 app.get("/", (req, res) => {
   res.send("API running 🚀");
 });
